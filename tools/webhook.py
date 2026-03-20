f627c4cdeb9c4e75ad5dba94294325960bb1ff3c
swakraft <hugo.beets2003@gmail.com>
2026-03-20 17:55:56 +0100

    updated webhook tool

--- tools/webhook.py
+++ tools/webhook.py
@@ -1,31 +1,33 @@
 from os import getenv
+from typing import Literal
 from dotenv import load_dotenv
 import hmac
 import hashlib
 import requests
+from src.settings import settings
 from src.v1.models.InvoiceWebhook import InvoiceWebhook
 
 load_dotenv(".env")
 
-def sign_webhook(id: str):
-    BYTES_API_KEY = bytes(getenv("OPENNODE_API_KEY"), "utf-8") # pyright: ignore[reportArgumentType]
-    invoice = InvoiceWebhook(
-        id=id,
-        callback_url="",
-        success_url="",
-        status="paid",
-        order_id="",
-        description="",
-        price=0,
-        fee=0,
-        auto_settle=False,
-        hashed_order="",
+def create_webhook(id: str, status: Literal["paid", "expired"] = "paid"):
+    return InvoiceWebhook(
+        id,
+        "",
+        "",
+        status,
+        "",
+        "",
+        0.0,
+        0.0,
+        False,
+        ""
     )
 
+def sign_webhook(invoice: InvoiceWebhook):
+    BYTES_API_KEY = bytes(getenv("OPENNODE_API_KEY"), "utf-8") # pyright: ignore[reportArgumentType]
     generated_signature = hmac.new(BYTES_API_KEY, invoice.id.encode('utf-8'), hashlib.sha256).hexdigest() # pyright: ignore[reportAttributeAccessIssue]
     invoice.hashed_order = generated_signature
     return invoice
 
-if __name__ == "__main__":
-    invoice = sign_webhook("")
-    print(requests.post("https://premium.swakraft.fr/webhook/opennode", invoice.__dict__).text)
+def send_webhook(invoice: InvoiceWebhook):
+    return requests.post(f"http://localhost:{settings.server_port}/webhook/opennode", invoice.__dict__)
