ee74adff8f9c4eb74ab0bdf6f6420397e6d674f3
swakraft <hugo.beets2003@gmail.com>
2026-03-20 14:48:55 +0100

    updated signing tool

--- tools/webhook.py
+++ tools/webhook.py
@@ -1,28 +1,31 @@
-from os import getenv, urandom
+from os import getenv
 from dotenv import load_dotenv
 import hmac
 import hashlib
 import requests
+from src.v1.models.InvoiceWebhook import InvoiceWebhook
 
 load_dotenv(".env")
 
-id = "40e540be-cf60-4cb4-8eff-3619521fc6ea"
-BYTES_API_KEY = bytes(getenv("OPENNODE_API_KEY"), "utf-8") # pyright: ignore[reportArgumentType]
-invoice = dict(
-    id=id,
-    callback_url="",
-    success_url="",
-    status="paid",
-    order_id="",
-    description="",
-    price=0,
-    fee=0,
-    auto_settle=False,
-    hashed_order="",
-)
+def sign_webhook(id: str):
+    BYTES_API_KEY = bytes(getenv("OPENNODE_API_KEY"), "utf-8") # pyright: ignore[reportArgumentType]
+    invoice = InvoiceWebhook(
+        id=id,
+        callback_url="",
+        success_url="",
+        status="paid",
+        order_id="",
+        description="",
+        price=0,
+        fee=0,
+        auto_settle=False,
+        hashed_order="",
+    )
 
-generated_signature = hmac.new(BYTES_API_KEY, invoice["id"].encode('utf-8'), hashlib.sha256).hexdigest() # pyright: ignore[reportAttributeAccessIssue]
+    generated_signature = hmac.new(BYTES_API_KEY, invoice.id.encode('utf-8'), hashlib.sha256).hexdigest() # pyright: ignore[reportAttributeAccessIssue]
+    invoice.hashed_order = generated_signature
+    return invoice
 
-invoice["hashed_order"] = generated_signature
-
-requests.post("https://premium.swakraft.fr/webhook/opennode", invoice)
+if __name__ == "__main__":
+    invoice = sign_webhook("")
+    print(requests.post("https://premium.swakraft.fr/webhook/opennode", invoice.__dict__).text)
